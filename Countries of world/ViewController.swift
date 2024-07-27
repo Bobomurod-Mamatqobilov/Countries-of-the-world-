ce47a394a3f2fc0ce1288478df5cf33e37da3755
Mirsaidov Bekzod <mirsaidovB406@gmail.com>
2024-07-28 01:16:21 +0500

    Initial Commit

---
+++ Countries of world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries of world
+//
+//  Created by Mamatqobilov Bobomurod on 28/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
